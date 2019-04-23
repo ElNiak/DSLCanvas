b854177f455993febe2ed77e436ef1aabe61d4f3
Antoine <antoine.rime@student.uclouvain.be>
2019-04-23 17:26:19 +0200

    Fix merge

--- main/scala/DSLStatic/Shape.scala
+++ main/scala/DSLStatic/Shape.scala
@@ -5,14 +5,10 @@ import DSLStatic.Color.Color
 abstract class Shape{
   var stroke : Stroke
   var color : Color
-<<<<<<< HEAD
   var x : Int
   var y : Int
   var canvasyElement : CanvasyElement
-=======
-  var x,y : Int
-  val canevasyElementModifier : CanvasyElementModifier[Shape]
->>>>>>> 394d4ee... Fix canvasy
+
 
   def stroke(col : Color.Value) : Unit
   def stroke(w : Int) : Unit
