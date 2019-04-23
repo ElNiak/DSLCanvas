99d558dcb8a8c95cb2f9a24536f9e3e8002b961f
Antoine <antoine.rime@student.uclouvain.be>
2019-04-23 16:22:25 +0200

    Start canvasy

--- main/scala/DSLStatic/Canvasy.scala
+++ main/scala/DSLStatic/Canvasy.scala
@@ -1,5 +1,19 @@
 package DSLStatic
 
-class Canvasy {
+import org.scalajs.dom
+import org.scalajs.dom.{document, html}
 
+import scala.collection.mutable.ListBuffer
+
+class Canvasy(c: html.Canvas) {
+  private val ctx = c.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+  private val list_groups = new ListBuffer[Array[Shape]]
+
+  def draw(): Unit = {
+    ???
+  }
+
+  def +=(group: Array[Shape]): Unit = {
+    list_groups.+=:(group)
+  }
 }
