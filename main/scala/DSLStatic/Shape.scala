5b03c6610d0a1de0877ab3375c25feac980989ea
antoinerime <antoine.rime@student.uclouvain.be>
2019-04-24 11:25:07 +0200

    Implcit class group

--- main/scala/DSLStatic/Shape.scala
+++ main/scala/DSLStatic/Shape.scala
@@ -2,29 +2,49 @@ package DSLStatic
 
 import DSLStatic.Color.Color
 
-trait Shape{
-  var stroke : Stroke
-  var color : Color
-  var x : Int
-  var y : Int
-  var canvasyElement : CanvasyElement
+trait Shape {
+  var stroke: Stroke
+  var color: Color
+  var x: Int
+  var y: Int
+  var canvasyElement: CanvasyElement
 
 
-  def stroke(col : Color.Value) : Unit
-  def stroke(w : Int) : Unit
-  def and(shape: Shape) : Shape
-  def and(groups: Groups) : Groups
-  def translateY(Y : Int) : Unit = {y += Y}
-  def translateX(X : Int) : Unit = {x += X}
+  def stroke(col: Color.Value): Unit
 
-  implicit class arrayShape(group: Array[Circle]) {
-    def translateY(Y: Int) : Unit = {group foreach(_ translateY(Y))}
-    def translateX(X: Int) : Unit = {group foreach(_ translateX(X))}
+  def stroke(w: Int): Unit
+
+  def and(shape: Shape): Shape
+
+  def and(groups: Groups): Groups
+
+  def translateY(Y: Int): Unit = {
+    y += Y
   }
+
+  def translateX(X: Int): Unit = {
+    x += X
+  }
+}
+
+
+
 
 object Shape {
   def and(shape: Shape) = {
+    val array = new Array[Shape]()
+  }
 
+  implicit class Group(group: Array[Circle]) {
+
+    def translateY(Y: Int) : Array[Circle] = {
+      group foreach(_ translateY(Y))
+      return group
+    }
+    def translateX(X: Int) : Array[Circle] = {
+      group foreach(_ translateX(X))
+      return group
+    }
   }
 
   def change(shape: Shape) = {
