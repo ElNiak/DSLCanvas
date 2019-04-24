eb62666980649cd86479dd37488022cf55e754b0
antoinerime <antoine.rime@student.uclouvain.be>
2019-04-24 11:44:00 +0200

    Define and method for shape

--- main/scala/DSLStatic/Shape.scala
+++ main/scala/DSLStatic/Shape.scala
@@ -8,15 +8,30 @@ trait Shape {
   var x: Int
   var y: Int
   var canvasyElement: CanvasyElement
+  var size : Int
 
 
   def stroke(col: Color.Value): Unit
 
   def stroke(w: Int): Unit
 
-  def and(shape: Shape): Shape
+  def size(s: Int): Unit = {
+    size = s
+  }
 
-  def and(groups: Groups): Groups
+  def and(shape: Shape): Array[Shape] = {
+    val array = new Array[Shape](2)
+    array(this)
+    array(shape)
+    return array
+  }
+
+  def and(groups: Array[Shape]): Array[Shape] = {
+    val array = new Array[Shape](1 + groups.length)
+    groups foreach (array(_))
+    array(this)
+    return array
+  }
 
   def translateY(Y: Int): Unit = {
     y += Y
@@ -31,8 +46,20 @@ trait Shape {
 
 
 object Shape {
-  def and(shape: Shape) = {
-    val array = new Array[Shape]()
+  def and(shape: Shape): Array[Shape] = {
+
+  }
+
+  implicit class Group(group: Array[Shape]) {
+
+    def translateY(Y: Int) : Array[Shape] = {
+      group foreach(_ translateY(Y))
+      return group
+    }
+    def translateX(X: Int) : Array[Shape] = {
+      group foreach(_ translateX(X))
+      return group
+    }
   }
 
   implicit class Group(group: Array[Circle]) {
@@ -42,6 +69,18 @@ object Shape {
       return group
     }
     def translateX(X: Int) : Array[Circle] = {
+      group foreach(_ translateX(X))
+      return group
+    }
+  }
+
+  implicit class Group(group: Array[Rectangle]) {
+
+    def translateY(Y: Int) : Array[Rectangle] = {
+      group foreach(_ translateY(Y))
+      return group
+    }
+    def translateX(X: Int) : Array[Rectangle] = {
       group foreach(_ translateX(X))
       return group
     }
