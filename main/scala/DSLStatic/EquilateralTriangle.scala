79479bf0166c2e39bbbb6109e373b7d64314e421
antoinerime <antoine.rime@student.uclouvain.be>
2019-05-01 16:23:42 +0200

    fix triangle

--- main/scala/DSLStatic/EquilateralTriangle.scala
+++ main/scala/DSLStatic/EquilateralTriangle.scala
@@ -5,7 +5,7 @@ import java.lang.Math
 case class EquilateralTriangle (X: Double, Y: Double, A: Double) extends Triangle {
   override var a: (Double, Double) = (X, Y)
   override var b: (Double, Double) = (X + A, Y)
-  override var c: (Double, Double) = (X + A*Math.sin(30), A*Math.cos(30))
+  override var c: (Double, Double) = (X + A*0.5, Y + A*Math.sqrt(3)/2)
   override var stroke: Stroke = new Stroke
   override var color: String = Color.red
   override var x: Double = X
